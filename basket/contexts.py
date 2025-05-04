140c93cf8cb0b6c6144037aa3d4d9a516938ff66
Damien <damienreganmusic@gmail.com>
2025-05-04 17:57:10 +0100

    Added product quantity input script for product detail page

--- basket/contexts.py
+++ basket/contexts.py
@@ -10,14 +10,14 @@ def basket_contents(request):
     product_count = 0
     basket = request.session.get('basket', {})
 
-    for item_id, quantity in basket.items():
+    for item_id, item_data in basket.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
-            total += quantity * product.price
-            product_count += quantity
+            total += item_data * product.price
+            product_count += item_data
             basket_items.append({
                 'item_id': item_id,
-                'quantity': quantity,
+                'quantity': item_data,
                 'product': product,
             })
 
@@ -28,7 +28,7 @@ def basket_contents(request):
                 product_count += quantity
                 basket_items.append({
                     'item_id': item_id,
-                    'quantity': quantity,
+                    'quantity': item_data,
                     'product': product,
                     'size': size,
                 })    
