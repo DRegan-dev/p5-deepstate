32c567d745b934284d6ad4b3f6e5e269ed9ddf18
Damien <damienreganmusic@gmail.com>
2025-06-25 21:57:21 +0100

    Updated accounts views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,35 +1,39 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 def login_view(request):
     if request.method == 'POST':
-        username = request.Post.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
+        form = UserLoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.Post.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
-            messages.success(request, 'Succesfully logged in!')
-            return redirect('home')
-        else:
-            messages.error(request, 'Invalid username or password')
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Succesfully logged in!')
+                return redirect('home')
+            else:
+                messages.error(request, 'Invalid username or password')
+    else:
+        form = UserLoginForm()
 
-    return render(request, 'accounts/login.html')
+    return render(request, 'accounts/login.html', {'form': form})
 
 def register_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             messages.success(request, 'Account created succesfully!')
             return redirect('home')
     else:
-        form = UserCreationForm
+        form = UserRegistrationForm()
 
     return render(request, 'accounts/register.html', {'form': form})
