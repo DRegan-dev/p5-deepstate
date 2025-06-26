3e1e582c6769d36fc3314d1dc0e326e30909cd8c
Damien <damienreganmusic@gmail.com>
2025-06-26 11:42:36 +0100

    Added admin dashboard and styling

--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -8,6 +8,25 @@ from checkout.models import OrderLineItem
 
 @login_required
 def admin_dashboard(request):
+    # Get statistics
     total_products = Product.objects.count()
     total_categories = Category.objects.count()
     total_sales = OrderLineItem.objects.aggregate(Sum(lineitem_total))['lineitem_total__sum'] or 0
+
+    #Get recent products
+    recent_products = Product.objects.orderby('-date_added')[:5]
+
+    # Get best-selling products
+    best_selling = OrderLineItem.objects.values('products__name').annotate(
+        total_sold=Sum('quantity')
+    ).orderby('-total_sold')[:5]
+
+    context = {
+        'total_products': total_products,
+        'total_categories': total_categories,
+        'total_sales': total_sales,
+        'recent_products': recent_products,
+        'best_selling': best_selling
+    }
+
+    return render(request, 'admin_dashboard/dashboard.html', context)
