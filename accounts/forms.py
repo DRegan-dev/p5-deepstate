d6b592e2be42cf86934b29ff0808180592e8e844
Damien <damienreganmusic@gmail.com>
2025-07-01 13:10:39 +0100

    Added UserProfileForm to account/forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import UserProfile
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User 
 
@@ -41,3 +42,33 @@ class UserLoginForm(AuthenticationForm):
             'class': 'form-control',
             'placeholder': 'Enter your Username'
         })
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs)
+        """
+        Add paceholders and classes, remove auto-generated labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_postcode': 'Postal Code',
+            'default_town_or_city': 'Town or City',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_county': 'County, State or Locality'
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus']= True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
