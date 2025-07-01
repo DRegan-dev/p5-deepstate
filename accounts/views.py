bbe8f17564292d6b6c851f4f15a3d03bc5a06823
Damien <damienreganmusic@gmail.com>
2025-07-01 12:04:51 +0100

    Added profile view

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import UserRegistrationForm, UserLoginForm
+from .models import UserProfile
+from checkout.models import Order
+from .forms import UserRegistrationForm, UserLoginForm, UserProfileForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -37,3 +40,32 @@ def register_view(request):
         form = UserRegistrationForm()
 
     return render(request, 'accounts/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    """
+    Display the user's profile.
+    """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+    else:
+        form = UserProfileForm(instance=profile)
+
+    orders = profile.orders.all()
+
+    template = 'accounts/profile.html'
+    context = {
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+        
+
