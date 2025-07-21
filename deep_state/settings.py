9c229e1f6696647016bfe4b8cf7e46556de7e443
Damien <damienreganmusic@gmail.com>
2025-07-21 16:27:17 +0100

    Debug False

--- deep_state/settings.py
+++ deep_state/settings.py
@@ -208,7 +208,7 @@ LOGGING = {
     'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
-            'format': '{level.name} {asctime} {module} {process:d} {thread:d} {message}',
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
             'style': '{',
         },
     },
