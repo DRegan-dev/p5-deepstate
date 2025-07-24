ec70d87d45cea37cd78ef2b45e88d47e27a0479d
Damien <damienreganmusic@gmail.com>
2025-07-24 11:12:02 +0100

    Added debug logging to resave_product_images command

--- products/management/commands/resave_product_images.py
+++ products/management/commands/resave_product_images.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand
 from products.models import Product
+import logging 
+logger = logging.getLogger(__name__)
 class Command(BaseCommand):
     help = 'Re-save all product images to trigger re-upload to S3'
 
@@ -9,7 +11,9 @@ class Command(BaseCommand):
             if product.image:
                 image = product.image
                 product.image = image
+                logger.info(f"Before: {product.image.url}")
                 product.save()
+                logger.info(f"After: {product.image.url}")
                 updated += 1
                 self.stdout.write(self.style.SUCCESS(f'Updated: {product.name}'))
         self.stdout.write(self.style.SUCCESS(f'Updated {updated} products'))
