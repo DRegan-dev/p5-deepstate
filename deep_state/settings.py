42ac2d2afd6a86df18aed30c174a7b0a3f430ae5
Damien <damienreganmusic@gmail.com>
2025-07-24 16:02:53 +0100

    debug=false

--- deep_state/settings.py
+++ deep_state/settings.py
@@ -146,10 +146,6 @@ if 'DATABASE_URL' in os.environ:
         AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'eu-north-1')
         AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-        AWS_S3_FILE_OVERWRITE = False
-        AWS_S3_MAX_MEMORY_SIZE = 8688608
-        AWS_SE_UPLOAD_PART_SIZE = 8688608
-
         STATICFILES_STORAGE = 'custom_storages.StaticStorage'
         STATICFILES_LOCATION = 'static'
         STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
@@ -158,8 +154,6 @@ if 'DATABASE_URL' in os.environ:
         MEDIAFILES_LOCATION = 'media'   
         MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
-        AWS_S3_FILE_OVERWRITE = False
-        AWS_DEFAULT_ACL = None
 
 
 else: 
