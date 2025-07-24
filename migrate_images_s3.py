ab0514a15fd834762cd9c639784f713a1627dfef
Damien <damienreganmusic@gmail.com>
2025-07-24 09:16:04 +0100

    fixed uppcase keyword agrument in migrate_images_s3.py

--- migrate_images_s3.py
+++ migrate_images_s3.py
@@ -10,7 +10,7 @@ def migrate_images_to_s3():
     s3_client = boto3.client(
         's3',
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-        AWS_SECRET_ACCESS_KEY=settings.AWS_SECRET_ACCESS_KEY,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
         region_name=settings.AWS_S3_REGION_NAME
     )
 
