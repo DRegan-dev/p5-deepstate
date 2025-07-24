af0617c44a46b6fc69a5e872bb45e1c48a15895d
Damien <damienreganmusic@gmail.com>
2025-07-24 09:12:42 +0100

    Added migrate_images_s3.py

--- deep_state/settings.py
+++ deep_state/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    "storages",
     'allauth',
     'allauth.account',
     'widget_tweaks',
@@ -199,6 +200,11 @@ WHITENOISE_STATIC_PREFIX = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AWS_ACCESS_KEY_ID =os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'eu-borth-1')
 
 
 # Default primary key field type
---
+++ migrate_images_s3.py
@@ -0,0 +1,34 @@
+import boto3
+from django.conf import settings
+from products.models import Product
+
+def migrate_images_to_s3():
+    if settings.DEBUG:
+        print("This script is intended for production only (DEBUG=FALSE).")
+        return
+    
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        AWS_SECRET_ACCESS_KEY=settings.AWS_SECRET_ACCESS_KEY,
+        region_name=settings.AWS_S3_REGION_NAME
+    )
+
+    products = Product.objects.filter(image__isnull=False)
+
+    for product in products:
+        image_field = product.image
+        s3_key = f"media/{image_field.name}"
+
+        try:
+            with image_field.open('rb') as image_file:
+                s3_client.upload_fileobj(
+                    image_file,
+                    settings.AWS_STORAGE_BUCKET_NAME,
+                    s3_key,
+                    ExtraArgs={'ACL': 'public-read'}
+    
+                )
+            print(f'uploaded {image_field.name} to s3')
+        except Exception as e:
+            print(f"Error uploading {image_field.name}: {e}")
